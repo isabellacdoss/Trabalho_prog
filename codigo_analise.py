0910a689544efd2d11a41a9026460749b12bcbd6
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-01 16:47:38 -0300

    Update codigo_analise.py

--- codigo_analise.py
+++ codigo_analise.py
@@ -5,10 +5,8 @@ print(os.listdir(os.getcwd()))
 
 dados = pd.read_csv("mcmv_financiado_fgts.csv", sep=";", encoding="ISO-8859-1")
 dados.head()
-dados.rename (columns = {'txt_municipio':'Município','txt_uf':'Estado','txt_regiao':'Região','num_ano_financiamento':'Ano','qtd_uh_financiadas':'Quantidade_de_unid_financiadas','vlr_financiamento':'Valor_financiado','vlr_subsidio': 'Valor_subsidiado'}, inplace = True)
+dados.rename (columns = {'txt_municipio':'Município','txt_uf':'Estado','txt_regiao':'Região','num_ano_financiamento':'Ano'}, inplace = True)
+dados.rename (columns = {'qtd_uh_financiadas':'Quantidade','vlr_financiamento':'Valor_financiado','vlr_subsidio': 'Valor_subsidiado'}, inplace = True)
 
-media = dados.Quantidade_de_unid_financiadas.mean()
-
-print (int(media))
 
 

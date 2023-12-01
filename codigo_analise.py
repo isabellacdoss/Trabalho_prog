303f9619d0b8adf8ae13fb2c92be6d6fa7b6dd17
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-01 16:08:38 -0300

    Update codigo_analise.py

--- codigo_analise.py
+++ codigo_analise.py
@@ -4,7 +4,6 @@ import pandas as pd
 print(os.listdir(os.getcwd()))
 
 dados = pd.read_csv("mcmv_financiado_fgts.csv", sep=";", encoding="ISO-8859-1")
-print(dados.head())
-
-#dados.rename (columns = {'txt_municipio': 'Município', 'txt_uf' : 'Estado'}, inplace = True)
-#print (dados)
+dados.head()
+dados.rename (columns = {'txt_municipio': 'Município', 'txt_uf' : 'Estado', 'txt_regiao': 'Região', 'num_ano_financiamento': 'Ano', 'qtd_uh_financiadas': 'Quantidade_de_unid._financiadas', 'vlr_financiamento': 'Valor_financiado', 'vlr_subsidio': 'Valor_subsidiado'}, inplace = True)
+print (dados)

38822954debb98330c6dc09ec3706677e9966e58
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-02 21:46:54 -0300

    Update codigo_analise.py

--- codigo_analise.py
+++ codigo_analise.py
@@ -1,15 +1,52 @@
-#Análise 1
+# Análise 1 - Média de unidades habitacionais financiadas por ano (2009 a 2023)
 
-def media (base, ano):
+def media_uni_habitacionais (base, ano):  
+
+    import numpy as np
+
     lista = []
     for i in range (len(base["Ano"])):
         if (base.at[i, 'Ano']) == ano:
             lista.append (base.at[i, 'Quantidade'])
-    return lista
-    
+            media_de_unid = np.mean(lista)
 
-#def media_anos (base, ano):
-    
+    return (media_de_unid) 
+
+# Análise 2 - Montante total do valor subsidiano por ano pelo governo 
+
+def montante_ano (base, ano):
+
+    somatorio = 0 
+    for i in range (len(base["Ano"])):
+        if (base.at[i, 'Ano']) == ano:
+            a = (base.at[i, 'Valor_subsidiado'])
+            somatorio += a
+
+    return (somatorio) 
+
+# Análise 3 - Determinação dos montante total por região do valor do financiamento (2009 a 2023)
+
+def montante_regiao (base, regiao):  
+
+    somatorio = 0 
+    for i in range (len(base["Região"])):
+        if (base.at[i, 'Região']) == regiao:
+            a = (base.at[i, 'Valor_financiamento'])
+            somatorio += a
+
+    return (somatorio) 
+
+# Análise 4 - Média dos montantes da análise anterior (3)
+
+#Identificação do estado que mais solicitou o financiamento de unidades habitacionais e o que menos solicitou durante o período de 2009 a 2023;
+
+
+#def estado (base):
+     
+    #estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+    #for i in range(len(base["Município"])):
+
+                   
 
     
 
@@ -31,11 +68,15 @@ def main():
 
     dados = dados.assign(Valor_financiado = dados['Valor_financiado'].str.replace(',','.').astype(float))
     dados = dados.assign(Valor_subsidiado = dados['Valor_subsidiado'].str.replace(',','.').astype(float))
+    ano = int(input("Digite o ano: \n "))
+    # regiao = input("Digite o nome da região: \n")
 
-    print(dados)
+    n = media_uni_habitacionais (dados,ano)
 
-    ano = int(input("Digite o ano:"))
-    media (dados, ano)
+    por_ano = montante_ano (dados, ano)
+    por_regiao = montante_regiao
+
+    print ("Média de unidades habitacionais no ano de", ano, ":" , n)
 
 main()
 

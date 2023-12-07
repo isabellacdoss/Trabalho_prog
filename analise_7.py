ef291e3e71c906912bd4c21f4176b13411dcf99e
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-07 00:57:08 -0300

    Create analise_7.py

---
+++ analise_7.py
@@ -0,0 +1,11 @@
+'''                    Município         Valor
+4304                Sï¿½o Paulo  2.996378e+09
+2237              Joï¿½o Pessoa  2.226886e+09
+3640             Rio de Janeiro  2.191540e+09
+4578               Uberlï¿½ndia  1.614169e+09
+4643    Valparaï¿½so de Goiï¿½s  1.399529e+09
+3583           Ribeirï¿½o Preto  1.317948e+09
+1630                  Goiï¿½nia  1.235892e+09
+1475           Feira de Santana  1.171320e+09
+787                Campo Grande  1.151931e+09
+4782  ï¿½guas Lindas de Goiï¿½s  1.135820e+09'''

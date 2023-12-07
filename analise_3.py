6a4faac4fd9a376c8501d4519cc8bc7db8d43009
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-07 00:56:45 -0300

    Create analise_3.py

---
+++ analise_3.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+
+'''O valor do montante do financiamento na região Norte foi de 12946852220.030006
+
+O valor do montante do financiamento na região Nordeste foi de 92040243169.12979
+
+O valor do montante do financiamento na região Centro-Oeste foi de 63795437310.07008
+
+O valor do montante do financiamento na região Sudeste foi de 256984119921.14108
+
+O valor do montante do financiamento na região Sul foi de 113083192208.50046'''
+

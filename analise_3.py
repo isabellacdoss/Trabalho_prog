833bda73f2d81be01de6738ff8b09d32ed6dd4bc
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-07 20:24:41 -0300

    Update analise_3.py

--- analise_3.py
+++ analise_3.py
@@ -1,13 +1,7 @@
 import matplotlib.pyplot as plt
 
+valor = [12946852220.03, 92040243169.13,63795437310.07,256984119921.14,113083192208.50 ]
+regioes = ['Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul']
 
-'''O valor do montante do financiamento na região Norte foi de 12946852220.030006
 
-O valor do montante do financiamento na região Nordeste foi de 92040243169.12979
-
-O valor do montante do financiamento na região Centro-Oeste foi de 63795437310.07008
-
-O valor do montante do financiamento na região Sudeste foi de 256984119921.14108
-
-O valor do montante do financiamento na região Sul foi de 113083192208.50046'''
 

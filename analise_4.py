e93c1932640ef4dd2a44e95cdb46d35899186074
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-07 20:24:45 -0300

    Update analise_4.py

--- analise_4.py
+++ analise_4.py
@@ -1,10 +1,8 @@
-''' A soma dos valores por estado é: [3037, 91836, 1372, 26464, 201136, 138976, 67719, 445158, 
-89701, 104815, 103275, 699513, 64168, 154347, 532971, 176833, 63344, 270560, 123881, 440762, 29244, 4714,
- 281194, 1494892, 81896, 19823, 55586] seguindo a ordem da lista dos estados ['Acre', 'Alagoas', 'Amapá', 
- 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul',
-   'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 
-   'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'] .   
+import matplotlib.pyplot as mpl
 
+valores = [3037, 91836, 1372, 26464, 201136, 138976, 67719, 445158,  89701, 104815, 103275, 699513, 64168, 154347, 532971, 176833, 63344, 270560, 123881, 440762, 29244, 4714, 281194, 1494892, 81896, 19823, 55586]
+estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul','Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal']
 
-O estado que menos solicitou financiamentos foi: Amapá , com 1372 financiamentos.
-O estado que mais solicitou financiamentos foi: São Paulo , com 1494892 financiamentos.'''
+# O estado que menos solicitou financiamentos foi o Amapá, com 1372 financiamentos.
+# O estado que mais solicitou financiamentos foi São Paulo, com 1494892 financiamentos.
+

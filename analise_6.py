1aaa2b5efb4fbae7d8f7ba6b1677f925bf832d0a
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-07 00:57:05 -0300

    Create analise_6.py

---
+++ analise_6.py
@@ -0,0 +1,89 @@
+## Norte
+
+'''O valor financiado pelo região Norte durante o ano de 2009 foi de 3319568246.700005
+O valor financiado pelo região Norte durante o ano de 2010 foi de 7232612229.750032
+O valor financiado pelo região Norte durante o ano de 2011 foi de 9017751325.800016
+O valor financiado pelo região Norte durante o ano de 2012 foi de 10438799575.34998
+O valor financiado pelo região Norte durante o ano de 2013 foi de 15078316833.750034
+O valor financiado pelo região Norte durante o ano de 2014 foi de 16303938826.199924
+O valor financiado pelo região Norte durante o ano de 2015 foi de 16205746722.000032
+O valor financiado pelo região Norte durante o ano de 2016 foi de 13859145791.999966
+O valor financiado pelo região Norte durante o ano de 2017 foi de 13381470175.949984
+O valor financiado pelo região Norte durante o ano de 2018 foi de 14879736703.05004
+O valor financiado pelo região Norte durante o ano de 2019 foi de 14998665477.900028
+O valor financiado pelo região Norte durante o ano de 2020 foi de 14554124529.299913
+O valor financiado pelo região Norte durante o ano de 2021 foi de 14064165188.999977
+O valor financiado pelo região Norte durante o ano de 2022 foi de 16454345219.099987
+O valor financiado pelo região Norte durante o ano de 2023 foi de 7902959285586.967'''
+
+# Nordeste
+
+'''O valor financiado pelo região Nordeste durante o ano de 2009 foi de 25516616248.649998
+O valor financiado pelo região Nordeste durante o ano de 2010 foi de 51298393879.499626
+O valor financiado pelo região Nordeste durante o ano de 2011 foi de 65913366249.900764
+O valor financiado pelo região Nordeste durante o ano de 2012 foi de 70944014138.39946
+O valor financiado pelo região Nordeste durante o ano de 2013 foi de 93383051984.25114
+O valor financiado pelo região Nordeste durante o ano de 2014 foi de 113756875526.09918
+O valor financiado pelo região Nordeste durante o ano de 2015 foi de 128144815107.44943
+O valor financiado pelo região Nordeste durante o ano de 2016 foi de 105664663864.04878
+O valor financiado pelo região Nordeste durante o ano de 2017 foi de 113240710260.45036
+O valor financiado pelo região Nordeste durante o ano de 2018 foi de 107563475950.79945
+O valor financiado pelo região Nordeste durante o ano de 2019 foi de 97857236256.44984
+O valor financiado pelo região Nordeste durante o ano de 2020 foi de 90076744283.3996
+O valor financiado pelo região Nordeste durante o ano de 2021 foi de 98071440364.49971
+O valor financiado pelo região Nordeste durante o ano de 2022 foi de 119642031568.95035
+O valor financiado pelo região Nordeste durante o ano de 2023 foi de 6801674236750.283'''
+
+#Centro-Oeste
+
+'''O valor financiado pelo região Centro-Oeste durante o ano de 2009 foi de 15593881546.200045
+O valor financiado pelo região Centro-Oeste durante o ano de 2010 foi de 35351377003.50005
+O valor financiado pelo região Centro-Oeste durante o ano de 2011 foi de 51475735807.7999
+O valor financiado pelo região Centro-Oeste durante o ano de 2012 foi de 55541162675.25037
+O valor financiado pelo região Centro-Oeste durante o ano de 2013 foi de 70660643210.54988
+O valor financiado pelo região Centro-Oeste durante o ano de 2014 foi de 84014507432.09967
+O valor financiado pelo região Centro-Oeste durante o ano de 2015 foi de 80732908699.05014
+O valor financiado pelo região Centro-Oeste durante o ano de 2016 foi de 72844034441.8498
+O valor financiado pelo região Centro-Oeste durante o ano de 2017 foi de 81950051104.79977
+O valor financiado pelo região Centro-Oeste durante o ano de 2018 foi de 72490147791.3001
+O valor financiado pelo região Centro-Oeste durante o ano de 2019 foi de 74269983623.24953
+O valor financiado pelo região Centro-Oeste durante o ano de 2020 foi de 70975115224.49977
+O valor financiado pelo região Centro-Oeste durante o ano de 2021 foi de 63600411669.000175
+O valor financiado pelo região Centro-Oeste durante o ano de 2022 foi de 69398887093.20052
+O valor financiado pelo região Centro-Oeste durante o ano de 2023 foi de 7183848825110.641'''
+
+# Sudeste
+
+'''O valor financiado pelo região Sudeste durante o ano de 2009 foi de 88947228732.00127
+O valor financiado pelo região Sudeste durante o ano de 2010 foi de 153164938422.90076
+O valor financiado pelo região Sudeste durante o ano de 2011 foi de 174957455242.351
+O valor financiado pelo região Sudeste durante o ano de 2012 foi de 191630079901.50107
+O valor financiado pelo região Sudeste durante o ano de 2013 foi de 215008370766.89807
+O valor financiado pelo região Sudeste durante o ano de 2014 foi de 225540312007.19748
+O valor financiado pelo região Sudeste durante o ano de 2015 foi de 251476479018.60083
+O valor financiado pelo região Sudeste durante o ano de 2016 foi de 263827892392.65173
+O valor financiado pelo região Sudeste durante o ano de 2017 foi de 321685525197.00024
+O valor financiado pelo região Sudeste durante o ano de 2018 foi de 329146176009.1543
+O valor financiado pelo região Sudeste durante o ano de 2019 foi de 314258802911.24963
+O valor financiado pelo região Sudeste durante o ano de 2020 foi de 321485539677.0021
+O valor financiado pelo região Sudeste durante o ano de 2021 foi de 324672361534.50226
+O valor financiado pelo região Sudeste durante o ano de 2022 foi de 369415588320.59937
+O valor financiado pelo região Sudeste durante o ano de 2023 foi de 4537530922299.419'''
+
+# Sul 
+
+'''O valor financiado pelo região Sul durante o ano de 2009 foi de 41746976239.649895
+O valor financiado pelo região Sul durante o ano de 2010 foi de 79660059434.25047
+O valor financiado pelo região Sul durante o ano de 2011 foi de 94660162079.25027
+O valor financiado pelo região Sul durante o ano de 2012 foi de 103301112193.05151
+O valor financiado pelo região Sul durante o ano de 2013 foi de 128457748727.8497
+O valor financiado pelo região Sul durante o ano de 2014 foi de 136144221890.55074
+O valor financiado pelo região Sul durante o ano de 2015 foi de 139877414835.59927
+O valor financiado pelo região Sul durante o ano de 2016 foi de 136597884991.19879
+O valor financiado pelo região Sul durante o ano de 2017 foi de 140265005333.8497
+O valor financiado pelo região Sul durante o ano de 2018 foi de 135482464283.39957
+O valor financiado pelo região Sul durante o ano de 2019 foi de 125893470649.19977
+O valor financiado pelo região Sul durante o ano de 2020 foi de 115057231956.44942
+O valor financiado pelo região Sul durante o ano de 2021 foi de 104147912454.14955
+O valor financiado pelo região Sul durante o ano de 2022 foi de 118351984901.09967
+O valor financiado pelo região Sul durante o ano de 2023 foi de 6483104022463.114'''

ca63b83c5dd5d70d46d7f83290f018f192e09c74
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-07 00:57:00 -0300

    Create analise_5.py

---
+++ analise_5.py
@@ -0,0 +1,10 @@
+'''A soma dos valores por município é: [2138, 43661, 1313, 23495, 44351, 36163, 1528, 65139, 
+26211, 27098, 55108, 42587, 6088, 78360, 52136, 6238, 22768, 120772, 11631, 50905, 6975, 4519,
+ 3659, 267143, 24058, 4687587, 55586] seguindo a ordem da lista dos estados ['Rio Branco', 'Maceió',
+'Macapá', 'Manaus', 'Salvador', 'Fortaleza', 'Vitória', 'Goiânia', 'São Luís', 'Cuiabá', 'Campo Grande',
+'Belo Horizonte', 'Belém', 'João Pessoa', 'Curitiba', 'Recife', 'Teresina', 'Rio de Janeiro', 'Natal',
+ 'Porto Alegre', 'Porto Velho', 'Boa Vista', 'Florianópolis', 'São Paulo', 'Aracaju', 'Palmas', 'Brasília'] .
+
+
+A capital que menos solicitou financiamentos foi: Macapá , com 1313 financiamentos.
+A capital que mais solicitou financiamentos foi: Palmas , com 4687587 financiamentos.'''

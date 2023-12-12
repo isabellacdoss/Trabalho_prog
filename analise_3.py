995c8360f3e0848a3814b0ef8dfd6f60b70234b3
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-12 13:59:37 -0300

    Delete analise_3.py

--- analise_3.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-valor = [12946852220.03, 92040243169.13,63795437310.07,256984119921.14,113083192208.50 ]
-regioes = ['Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul']
-
-
-

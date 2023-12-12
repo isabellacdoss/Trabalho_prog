78bac4eb11192743ba100c7a509798405bf873da
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-11 22:06:32 -0300

    Update analise_5.py

--- analise_5.py
+++ analise_5.py
@@ -1,10 +1,16 @@
-'''A soma dos valores por município é: [2138, 43661, 1313, 23495, 44351, 36163, 1528, 65139, 
-26211, 27098, 55108, 42587, 6088, 78360, 52136, 6238, 22768, 120772, 11631, 50905, 6975, 4519,
- 3659, 267143, 24058, 4687587, 55586] seguindo a ordem da lista dos estados ['Rio Branco', 'Maceió',
-'Macapá', 'Manaus', 'Salvador', 'Fortaleza', 'Vitória', 'Goiânia', 'São Luís', 'Cuiabá', 'Campo Grande',
-'Belo Horizonte', 'Belém', 'João Pessoa', 'Curitiba', 'Recife', 'Teresina', 'Rio de Janeiro', 'Natal',
- 'Porto Alegre', 'Porto Velho', 'Boa Vista', 'Florianópolis', 'São Paulo', 'Aracaju', 'Palmas', 'Brasília'] .
+# Análise 5 - Avaliação da disparidade na quantidade de unidades habitacionais contratadas pelas capitais dos estados brasileiros durante os anos de 2009 a 2023
 
 
-A capital que menos solicitou financiamentos foi: Macapá , com 1313 financiamentos.
-A capital que mais solicitou financiamentos foi: Palmas , com 4687587 financiamentos.'''
+import matplotlib.pyplot as mpl
+
+capitais = ['Rio Branco', 'Maceió','Macapá', 'Manaus', 'Salvador', 'Fortaleza', 'Vitória', 'Goiânia', 'São Luís', 'Cuiabá', 'Campo Grande', 'Belo Horizonte', 'Belém', 'João Pessoa', 'Curitiba', 'Recife', 'Teresina', 'Rio de Janeiro', 'Natal','Porto Alegre', 'Porto Velho', 'Boa Vista', 'Florianópolis', 'São Paulo', 'Aracaju', 'Palmas', 'Brasília']
+quantidade = [2138, 43661, 1313, 23495, 44351, 36163, 1528, 65139, 26211, 27098, 55108, 42587, 6088, 78360, 52136, 6238, 22768, 120772, 11631, 50905, 6975, 4519,3659, 267143, 24058, 4687587, 55586] 
+
+mpl.figure(figsize = (15,10))
+mpl.pie(quantidade, labels = capitais, autopct = '%1.1f%%', shadow = True, startangle = 95)
+
+mpl.title("Porcentagem de financiamentos por capitail brasileira")
+
+mpl.show()
+
+

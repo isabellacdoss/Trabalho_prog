9e823ba34ff0c1fa77086ac37d1a6cf3e3bc9f81
isabellacdoss <isabella.c.santos@edu.ufes.br>
2023-12-12 15:16:19 -0300

    Update graficos.py

--- graficos.py
+++ graficos.py
@@ -3,6 +3,7 @@
 def grafico_1 ():
 
     import matplotlib.pyplot as mpl
+    import numpy as np
 
     # dados retirados da análise da base 
 
@@ -21,10 +22,10 @@ def grafico_1 ():
 def grafico_2 ():
 
     import matplotlib.pyplot as mpl
+    import numpy as np 
 
     valores = [46802.09, 151253.0, 139322.99, 205832.32, 156336.89, 217680.92, 254939.85, 219517.03, 150234.44, 124757.51, 117391.95, 201072.62, 350850.5, 240661.91, 40507.9]
     anos = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
-
 
     mpl.title('Valor subsidiado pelo Governo por ano')
     mpl.plot(anos, valores)
@@ -32,6 +33,7 @@ def grafico_2 ():
     mpl.plot(anos, valores, color = "red")
 
     mpl.ylim(30000, 360000)  # para que os valores do gráfico fiquem nesse intervalo 
+    mpl.xlim(2009,2023)
 
     mpl.xlabel('Anos')
     mpl.ylabel('Valor em real')
@@ -71,6 +73,7 @@ def grafico_5 ():
     mpl.show()
 
 def grafico_6 ():
+
     import matplotlib.pyplot as mpl
     import numpy as np
 
@@ -116,15 +119,143 @@ def grafico_7 (numero):
     ax.set(title="As 10 cidades que mais receberma subsídio do Governo durante 2009 e 2023", ylabel="Valor em bilhões")
     mpl.show()
 
-'''def grafico_8 ():
+def grafico_8 ():
+    '''Região        Ano 
+Centro-Oeste  2009     325
+              2010     338
+              2011     350
+              2012     343
+              2013     364
+              2014     368
+              2015     382
+              2016     369
+              2017     350
+              2018     343
+              2019     326
+              2020     315
+              2021     300
+              2022     274
+              2023     294
+Nordeste      2009    1023
+              2010    1014
+              2011     977
+              2012     959
+              2013    1002
+              2014    1033
+              2015    1046
+              2016     961
+              2017     914
+              2018     809
+              2019     797
+              2020     632
+              2021     631
+              2022     691
+              2023     683
+Norte         2009     152
+              2010     161
+              2011     166
+              2012     173
+              2013     191
+              2014     198
+              2015     199
+              2016     172
+              2017     153
+              2018     148
+              2019     139
+              2020     121
+              2021     130
+              2022     134
+              2023     127
+Sudeste       2009    1361
+              2010    1363
+              2011    1327
+              2012    1296
+              2013    1319
+              2014    1326
+              2015    1296
+              2016    1219
+              2017    1191
+              2018    1090
+              2019    1156
+              2020    1091
+              2021    1097
+              2022    1085
+              2023    1049
+Sul           2009    1014
+              2010    1075
+              2011    1073
+              2012    1065
+              2013    1090
+              2014    1081
+              2015    1064
+              2016     980
+              2017     934
+              2018     896
+              2019     887
+              2020     837
+              2021     784
+              2022     736
+              2023     705'''
 
 def grafico_9 ():
+    import matplotlib.pyplot as mpl
+    import numpy as np
 
-def grafico_10 ():'''
+    anos = [2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023]
+    norte = [725414897.8499999, 2051186392.0500002, 1112260653.900005, 2689378411.7999854, 4088328989.099965, 3890385196.949994, 3606700406.8499737, 2427659086.350007, 2551901427.59999, 3341421521.549974, 2961465200.399985, 2994608088.4499865, 2890771038.599986, 2805990904.5000086, 2246042432.399998]
+    nordeste = [8838175475.849955, 19268851781.25, 10894153582.349653, 23179690733.85074, 30404388110.1003, 34293216066.900658, 35700759714.00043, 27262751659.650135, 32656114122.000782, 34563644946.15071, 27520914099.450592, 25036410741.300278, 28086165897.299835, 31874926759.949894, 25764633630.750084]
+    centro_oeste = [4878445992.899983, 10788261285.0, 6678951113.099811, 15805814019.000036, 20065394146.94999, 23490491525.69983, 19851779560.499855, 15093353944.199783, 19878231439.49989, 18914074150.04995, 17370077274.899815, 15965407783.049847, 12583607758.649998, 13171215207.74996, 9628956944.99998]
+    sudeste= [21017598702.300026, 39183235434.900024, 18345236543.249702, 47450452447.34978, 50298283055.25027, 48174868530.45041, 46897781370.45006, 40461697148.25028, 58160429417.09962, 67259388393.59922, 53566528548.14988, 56780171799.449615, 46508609017.34995, 41296387404.29975, 27608987053.949757]
+    sul = [11966692348.799967, 23689609628.700005, 10771412181.899574, 28979535699.15003, 34815926282.99978, 32051365487.249706, 28210988352.59931, 26182376822.10025, 27760848508.650368, 29916563850.750294, 23038250226.75012, 21733383114.750034, 16382098681.050226, 15835169091.150215, 10532744155.049953]
+
+
+    mpl.figure(figsize = (25,20))
+    mpl.plot(anos, norte, color = "red", label = "Norte")
+    mpl.plot(anos, nordeste, color = "yellow",label = "Nordeste")
+    mpl.plot(anos, centro_oeste, color = "orange",label = "Centro-Oeste")
+    mpl.plot(anos, sudeste, color = "green",label = "Sudeste")
+    mpl.plot(anos, sul, color = "brown",label = "SuL")
+
+    mpl.ylim(100000000, 70000000000)  #valores do eixo y
+
+    mpl.title("Valor subsidiado por região e ano através do programa Minha Casa, Minha Vida")
+    mpl.grid(True)
+    mpl.box(True)
+    mpl.legend()
+    mpl.xlabel("Anos")
+    mpl.ylabel("Valor em bilhões de reais")
+
+    mpl.show()
+   
+#def grafico_10 ():
+
+
 
 def main():
-    grafico_1 ()
 
+    n_analise = int(input("Digite o número da análise: \n"))
+
+    if n_analise == 1:
+        grafico_1()
+    elif n_analise == 2:
+        grafico_2()
+    elif n_analise ==3:
+        grafico_3()
+    elif n_analise ==4:
+        grafico_4()
+    elif n_analise ==5:
+        grafico_5()
+    elif n_analise ==6:
+        grafico_6()
+    elif n_analise ==7:
+        grafico_7()
+    elif n_analise ==8:
+        grafico_8()
+    else:
+        grafico_9()
+    #else:
+        #grafico_10()
+        
 main()
 
 
